7eabca125daf745fe145735b03bd18161c355a92
김도균 <ehrbs4789@gmail.com>
2023-03-21 15:54:21 +0900

    영화감독 숌.py

---
+++ Python/Level11_브루트 포스/BOJ_1436.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+number = 666
+count = 0
+
+while True:
+    if '666' in str(number):
+        count += 1
+    if count == N:
+        print(number)
+        break
+    
+    number += 1
